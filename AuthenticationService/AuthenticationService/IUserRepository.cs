1047fe2af7e61157f36b352df7e1f30a4ab7e722
HelenArkh <alena.arkh@gmail.com>
2021-08-29 17:48:47 +0300

    Модуль 33 без итогового практического задания

---
+++ AuthenticationService/AuthenticationService/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService
+{
+   public interface IUserRepository
+    {
+        IEnumerable<User> GetAll();
+
+        User GetByLogin(string login);
+    }
+}
